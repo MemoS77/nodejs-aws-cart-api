fd3d82497591c43147465e210c6b265e1b6902a9
Memo <memosoft@ya.ru>
2024-07-21 13:39:24 +0300

    feat: work with users

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,28 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
 
-import { v4 } from 'uuid';
+import { v4 } from 'uuid'
 
-import { User } from '../models';
+import { User } from '../models'
+import { DBService } from '../../db/db.service'
 
 @Injectable()
 export class UsersService {
-  private readonly users: Record<string, User>;
+  constructor(private dbService: DBService) {}
 
-  constructor() {
-    this.users = {}
+  async findOne(userId: string): Promise<User> {
+    const user = await this.dbService.query(
+      `SELECT id, name, created_at FROM users WHERE id = $1 limit 1`,
+      [userId],
+    )
+
+    return user.rows[0]
   }
 
-  findOne(userId: string): User {
-    return this.users[ userId ];
+  async createOne({ name }: User): Promise<User> {
+    const id = v4()
+    const newUser: User = {
+      id: name || id,
+      name,
+      created_at: new Date().toISOString(),
+    }
+
+    await this.dbService.query(
+      `INSERT INTO users (id, name, created_at) VALUES ($1, $2, $3)`,
+      [newUser.id, newUser.name, newUser.created_at],
+    )
+    return newUser
   }
-
-  createOne({ name, password }: User): User {
-    const id = v4();
-    const newUser = { id: name || id, name, password };
-
-    this.users[ id ] = newUser;
-
-    return newUser;
-  }
-
 }
